9b4a652271286d8d7099de4a195ad2fa03b6db2c
Adam Plesca <116078864+adamplesca@users.noreply.github.com>
2023-12-08 12:09:18 +0000

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,31 @@
+//Created by Adam Plesca
+function btnClick() {
+    const btn = document.getElementById("btn");
+    $(document).ready(function () {
+        $.getJSON("../json/sale.json", function (data) {
+            const location = $("#items"); 
+            location.empty(); 
+            const shuffledData = shuffleArray(data);
+
+            for (let i = 0; i < 3; i++) {
+                const product = shuffledData[i]; 
+                if (product) {
+                    //appends HTML elements to the "items" element to display product details
+                    location.append($("<div class='name' id='name" + i + "'></div>"));
+                    $("#name" + i).append($("<p></p>").text(product.details.brand))
+                    $("#name" + i).append($("<p></p>").text(product.name))
+                    $("#name" + i).append($("<p></p>").html("<s>€" + product.oldPrice + "</s>"));
+                    $("#name" + i).append($("<p></p>").text("€" + product.price))
+                    $("#name" + i).append($("<img src='../" + product.image + "'>"))
+                }
+            }
+        });
+    });
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const ranNum = Math.floor(Math.random() * (i + 1));
+            [array[i], array[ranNum]] = [array[ranNum], array[i]];
+        }
+        return array;
+    }
+}

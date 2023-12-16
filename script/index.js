f686566fbee503ae2f07e2e53e326d39bd0a1d99
Adam Plesca <116078864+adamplesca@users.noreply.github.com>
2023-12-16 15:31:50 +0000

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,21 +1,23 @@
 //Created by Adam Plesca
 function btnClick() {
     const btn = document.getElementById("btn");
+    //jQuery waits for document to be ready
     $(document).ready(function () {
+       //makes AJAX call to load the json data from the file
         $.getJSON("data/sale.json", function (data) {
-
+            //gets location of where products should be
             const location = $("#items"); 
+            //clears location in case products are already there, to maintain only 3 elements at a time on the webpage
             location.empty(); 
+            //call function from lower to shuffle json data
             const shuffledData = shuffleArray(data);
-
+            //prints out the first 3 json elements which have already been shuffled
             for (let i = 0; i < 3; i++) {
                 const product = shuffledData[i]; 
                 if (product) {
-
                     //json index mismatch fix
                     getCorrectIndex(product, function(index) 
                     {
-                        
                         //appends HTML elements to the "items" element to display product details
                         location.append($("<div class='name' id='name" + i + "' onclick='viewProduct(" + index + ")'></div>"));
                         $("#name" + i).append($("<p></p>").text(product.details.brand))
@@ -25,21 +27,12 @@ function btnClick() {
                         $("#name" + i).append($("<img class='indexImg' src='../images/" + product.image + "'>"))
 
                     });
-                    
-
-                    /*
-                    //appends HTML elements to the "items" element to display product details
-                    location.append($("<div class='name' id='name" + i + "' onclick='viewProduct(" + i + ")'></div>"));
-                    $("#name" + i).append($("<p></p>").text(product.details.brand))
-                    $("#name" + i).append($("<p></p>").text(product.name))
-                    $("#name" + i).append($("<p></p>").html("<s>€" + product.oldPrice + "</s>"));
-                    $("#name" + i).append($("<p></p>").text("€" + product.price))
-                    $("#name" + i).append($("<img class='indexImg' src='../images/" + product.image + "'>"))
-                    */
                 }
             }
         });
     });
+    //function to shuffle json file, traverses the json file backwards, and generates a random index "ranNum" then swaps the two points and returns the shuffled array/json file
+    //eg i=15 ranNum = 12. the 15 element in the json file gets placed in the 12 postion, and will do this for each element it iterates until the for loop reaches 0
     function shuffleArray(array) {
         for (let i = array.length - 1; i > 0; i--) {
             const ranNum = Math.floor(Math.random() * (i + 1));
